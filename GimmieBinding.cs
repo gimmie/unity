edd3c0a6075a941dc5810bed7fc776b6e9a7f7f0
KeangMac <skeang@gmail.com>
2014-09-30 18:44:58 +0800

    Fix classname bug for 1.8.4 from Touchten

--- GimmieBinding.cs
+++ GimmieBinding.cs
@@ -22,45 +22,41 @@ public class GimmieBinding : MonoBehaviour {
 	[DllImport ("__Internal")]
 	private static extern void NativeIOSTriggerEvent(string eventname);
 	#endif
-
+	
 	public static void InitGimmie(){
 		#if UNITY_IPHONE
 		Debug.Log("Login to gimmie");
 		NativeIOSBindGimmieNotification();
 		NativeIOSAnonymousLogin();
 		#endif
-
+		
 		#if UNITY_ANDROID
 		AndroidJNI.AttachCurrentThread();
 		AndroidJavaClass player = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
 		AndroidJavaObject activity = player.GetStatic<AndroidJavaObject>("currentActivity");
+		
+		AndroidJavaClass gimmieComponent = new AndroidJavaClass("com.gimmie.GimmieComponents");
+		gimmieComponent.CallStatic("registerUnityHandler");
 
-		AndroidJavaClass gimmieComponents = new AndroidJavaClass("com.gimmie.components.GimmieComponents");
-		gimmieComponents.CallStatic("registerUnityHandler");
-
-		AndroidJavaObject componentInstance = gimmieComponents.CallStatic<AndroidJavaObject>("getInstance", activity);
-		AndroidJavaObject service = componentInstance.Call<AndroidJavaObject>("getGimmie");
-
-		service.Call("updateContext", activity);
-		service.Call("login");
+		// Auto call login.
+		gimmieComponent.CallStatic<AndroidJavaObject>("getInstance", activity);
 		#endif
 	}
-
+	
 	public static void Login(string user) {
 		Debug.Log("Login to gimmie");
 
 		#if UNITY_IPHONE
 		NativeIOSLogin(user);
 		#endif
-
+		
 		#if UNITY_ANDROID
 		AndroidJavaClass player = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
 		AndroidJavaObject activity = player.GetStatic<AndroidJavaObject>("currentActivity");
-
-		AndroidJavaClass gimmieComponents = new AndroidJavaClass("com.gimmie.components.GimmieComponents");
-		AndroidJavaObject componentInstance = gimmieComponents.CallStatic<AndroidJavaObject>("getInstance", activity);
+		
+		AndroidJavaClass gimmieComponent = new AndroidJavaClass("com.gimmie.GimmieComponents");
+		AndroidJavaObject componentInstance = gimmieComponent.CallStatic<AndroidJavaObject>("getInstance", activity);
 		AndroidJavaObject service = componentInstance.Call<AndroidJavaObject>("getGimmie");
-
 		service.Call ("loginAndTransferFromGuest", user, "", "");
 		#endif
 	}
@@ -73,11 +69,10 @@ public class GimmieBinding : MonoBehaviour {
 		#if UNITY_ANDROID
 		AndroidJavaClass player = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
 		AndroidJavaObject activity = player.GetStatic<AndroidJavaObject>("currentActivity");
-
-		AndroidJavaClass gimmieComponents = new AndroidJavaClass("com.gimmie.components.GimmieComponents");
-		AndroidJavaObject componentInstance = gimmieComponents.CallStatic<AndroidJavaObject>("getInstance", activity);
+		
+		AndroidJavaClass gimmieComponent = new AndroidJavaClass("com.gimmie.GimmieComponents");
+		AndroidJavaObject componentInstance = gimmieComponent.CallStatic<AndroidJavaObject>("getInstance", activity);
 		AndroidJavaObject service = componentInstance.Call<AndroidJavaObject>("getGimmie");
-
 		service.Call ("logout");
 		#endif
 	}
@@ -90,54 +85,52 @@ public class GimmieBinding : MonoBehaviour {
 		#if UNITY_ANDROID
 		AndroidJavaClass player = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
 		AndroidJavaObject activity = player.GetStatic<AndroidJavaObject>("currentActivity");
-
-		AndroidJavaClass gimmieComponents = new AndroidJavaClass("com.gimmie.components.GimmieComponents");
-		AndroidJavaObject componentInstance = gimmieComponents.CallStatic<AndroidJavaObject>("getInstance", activity);
+		
+		AndroidJavaClass gimmieComponent = new AndroidJavaClass("com.gimmie.GimmieComponents");
+		AndroidJavaObject componentInstance = gimmieComponent.CallStatic<AndroidJavaObject>("getInstance", activity);
 		AndroidJavaObject service = componentInstance.Call<AndroidJavaObject>("getGimmie");
-
 		String user = service.Call<String> ("getUser");
 		return user.StartsWith("guest:", true, null);
 		#endif
 	}
-
+	
 	public static void UpdateCountry(string countryCode) {
 		#if UNITY_IPHONE
 		NativeIOSUpdateGimmieCountry(countryCode);
 		#endif
-
+		
 		#if UNITY_ANDROID
 		AndroidJavaClass player = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
 		AndroidJavaObject activity = player.GetStatic<AndroidJavaObject>("currentActivity");
-
-		AndroidJavaClass gimmieComponents = new AndroidJavaClass("com.gimmie.components.GimmieComponents");
-		AndroidJavaObject componentInstance = gimmieComponents.CallStatic<AndroidJavaObject>("getInstance", activity);
+				
+		AndroidJavaClass gimmieComponent = new AndroidJavaClass("com.gimmie.GimmieComponents");
+		AndroidJavaObject componentInstance = gimmieComponent.CallStatic<AndroidJavaObject>("getInstance", activity);
 		AndroidJavaObject service = componentInstance.Call<AndroidJavaObject>("getGimmie");
-		
 		service.Call ("setCountry", countryCode);
 		#endif
 	}
-
+	
 	public static void ShowGimmieRewardsCatalogue(){
 		#if UNITY_IPHONE
 		NativeIOSShowGimmieRewards();
 		#elif UNITY_ANDROID
 		AndroidJavaClass player = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
 		AndroidJavaObject activity = player.GetStatic<AndroidJavaObject>("currentActivity");
-
-		AndroidJavaClass components = new AndroidJavaClass("com.gimmie.components.GimmieComponents");
+		
+		AndroidJavaClass components = new AndroidJavaClass("com.gimmie.GimmieComponents");
 		components.CallStatic("showRewardCatalogue", activity);
 		#endif
 	}
-
+	
 	public static void TriggerGimmieEvent(string eventName){
 		#if UNITY_IPHONE
 		NativeIOSTriggerEvent(eventName);
 		#elif UNITY_ANDROID
-		AndroidJavaClass components = new AndroidJavaClass("com.gimmie.components.GimmieComponents");
+		AndroidJavaClass components = new AndroidJavaClass("com.gimmie.GimmieComponents");
 		components.CallStatic("triggerEvent", eventName);
 		#endif
 	}
-
+	
 	public void HandleNeedLogin(string message) {
 		BroadcastMessage ("GimmieNeedLogin", null, SendMessageOptions.DontRequireReceiver);
 	}
